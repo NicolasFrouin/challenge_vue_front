f1d8473dfb507faf05093254d5cb0b873de6b3ca
NicolasFrouin <frouinnicolas@gmaiL.com>
2024-06-06 17:26:52 +0200

    add the component library 'element-plus'

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
+import "element-plus/dist/index.css";
 import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+import fr from "element-plus/es/locale/lang/fr";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+app.use(ElementPlus, { locale: fr });
 app.use(createPinia());
 app.use(router);
 
