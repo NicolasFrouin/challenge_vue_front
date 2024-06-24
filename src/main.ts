aa9e2b54bf6e32464305f74c24f10f4d1e13b29c
NicolasFrouin <frouinnicolas@gmaiL.com>
2024-06-24 13:10:31 +0200

    update eslint config

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
-import "element-plus/dist/index.css";
-import "./assets/main.css";
+import './assets/main.css';
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import ElementPlus from "element-plus";
-import fr from "element-plus/es/locale/lang/fr";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App);
 
-app.use(ElementPlus, { locale: fr });
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');
