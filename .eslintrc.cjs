f0b28acdb6bbf20b73f9c1399eeb968640ba03d6
NicolasFrouin <frouinnicolas@gmaiL.com>
2024-06-24 17:35:28 +0200

    first front

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,13 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: ['prettier', 'plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-airbnb-with-typescript'],
+  extends: [
+    'prettier',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-airbnb-with-typescript',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
@@ -16,8 +22,9 @@ module.exports = {
     'linebreak-style': 'off',
     'no-param-reassign': 'off',
     'class-methods-use-this': 'off',
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'prettier/prettier': ['error', { endOfLine: 'auto', objectCurlyNewline: false }],
     'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'vue/object-curly-newline': ['error', { multiline: true, consistent: true }],
     'vue/html-self-closing': ['error', { html: { normal: 'never', void: 'always' } }],
   },
 };
