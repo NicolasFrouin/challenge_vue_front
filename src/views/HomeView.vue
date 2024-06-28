5296626aca5f3d70852590314fc1e6fb2c890fcc
NicolasFrouin <frouinnicolas@gmaiL.com>
2024-06-28 15:06:24 +0200

    add loading when fetching products on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,37 @@
 <script setup lang="ts">
 import { ProductList } from '@/components/product';
+import useAppState from '@/stores/state';
 import { LineOf, type Product } from '@/types';
+import { useRefStore } from '@/utils/refStore';
 import type { AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 import { onMounted, ref } from 'vue';
 
 const products = ref<Product[]>([]);
 
+const { setLoading } = useRefStore(useAppState());
+
 async function fetchProducts() {
+  setLoading(true);
   const axiosConfig: AxiosRequestConfig = {
     method: 'GET',
     url: 'https://fakestoreapi.com/products',
   };
-  await axios(axiosConfig).then(
-    (response) => {
-      products.value = response.data.map((prod: Product) => ({
-        ...prod,
-        name: prod.title,
-        slug: (prod.title as string).replace(/\s+/g, '-').toLowerCase(),
-      }));
-    },
-    (err) => {
-      products.value = [];
-      throw new Error(err);
-    },
-  );
+  await axios(axiosConfig)
+    .then(
+      (response) => {
+        products.value = response.data.map((prod: Product) => ({
+          ...prod,
+          name: prod.title,
+          slug: (prod.title as string).replace(/\s+/g, '-').toLowerCase(),
+        }));
+      },
+      (err) => {
+        throw new Error(err);
+      },
+    )
+    .catch(() => (products.value = []))
+    .finally(() => setLoading(false));
 }
 
 onMounted(() => {
