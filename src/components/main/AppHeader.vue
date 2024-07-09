67ca05a74a633511549e582e4536e9556f550183
YvesMu <95441649+YvesMu@users.noreply.github.com>
2024-07-09 12:45:21 +0200

    Update AppHeader.vue
    Changement du menu pour qu'il y ait Edition limité et les boissons classiques

--- src/components/main/AppHeader.vue
+++ src/components/main/AppHeader.vue
@@ -56,306 +56,46 @@ const content: Node = {
   isLeaf: false,
   children: [
     {
-      key: 'WOMEN',
+      key: 'LIMITED_EDITION',
       value: {
-        label: 'Women',
-        counter: 515,
-        banner: 'https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/glasses.png',
-        bannerTitle: 'The world in a new light',
+        label: 'Edition limitée',
+        counter: 150,
+        banner: 'https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/limited_edition.png',
+        bannerTitle: 'Exclusive Limited Editions',
       },
       isLeaf: false,
       children: [
         {
-          key: 'ALL_WOMEN',
-          value: { label: "All Women's", counter: 515, link: '#' },
+          key: 'ALL_LIMITED_EDITION',
+          value: { label: 'All Limited Editions', counter: 150, link: '#' },
           isLeaf: true,
         },
         {
-          key: 'CATEGORIES',
-          value: { label: 'Categories', counter: 178 },
-          isLeaf: false,
-          children: [
-            {
-              key: 'ALL_CATEGORIES',
-              value: { label: 'All Categories', counter: 178, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'CLOTHING',
-              value: { label: 'Clothing', counter: 30, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'SHOES',
-              value: { label: 'Shoes', counter: 28, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'ACCESSORIES',
-              value: { label: 'Accessories', counter: 56, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'WEARABLES',
-              value: { label: 'Wearables', counter: 12, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'FOOD_DRINKS',
-              value: { label: 'Food & Drinks', counter: 52, link: '#' },
-              isLeaf: true,
-            },
-          ],
-        },
-        {
-          key: 'ACTIVITIES',
-          value: { label: 'Activities', counter: 239 },
-          isLeaf: false,
-          children: [
-            {
-              key: 'ALL_ACTIVITIES',
-              value: { label: 'All Activities', counter: 239, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'FITNESS',
-              value: { label: 'Fitness', counter: 83, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'PILATES',
-              value: { label: 'Pilates', counter: 65, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'TRAINING',
-              value: { label: 'Training', counter: 21, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'CARDIO_WORKOUT',
-              value: { label: 'Cardio Workout', counter: 50, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'YOGA',
-              value: { label: 'Yoga', counter: 20, link: '#' },
-              isLeaf: true,
-            },
-          ],
-        },
-        {
-          key: 'DEALS',
-          value: { label: 'Deals', counter: 98 },
-          isLeaf: false,
-          children: [
-            {
-              key: 'ALL_DEALS',
-              value: { label: 'All Deals', counter: 98, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'OUTLET',
-              value: { label: 'Outlet', counter: 98, link: '#' },
-              isLeaf: true,
-            },
-          ],
+          key: 'SPECIAL_RELEASE',
+          value: { label: 'Special Releases', counter: 75, link: '#' },
+          isLeaf: true,
         },
       ],
     },
     {
-      key: 'MEN',
+      key: 'CLASSICS',
       value: {
-        label: 'Men',
+        label: 'Classiques',
         counter: 364,
-        banner: 'https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/watch.png',
-        bannerTitle: 'New in designer watches',
+        banner: 'https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/classics.png',
+        bannerTitle: 'Timeless Classics',
       },
       isLeaf: false,
       children: [
         {
-          key: 'ALL_MEN',
-          value: { label: "All Men's", counter: 364, link: '#' },
+          key: 'ALL_CLASSICS',
+          value: { label: 'All Classics', counter: 364, link: '#' },
           isLeaf: true,
         },
         {
-          key: 'CATEGORIES',
-          value: { label: 'Categories', counter: 164 },
-          isLeaf: false,
-          children: [
-            {
-              key: 'ALL_CATEGORIES',
-              value: { label: 'All Categories', counter: 164, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'CLOTHING',
-              value: { label: 'Clothing', counter: 41, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'SHOES',
-              value: { label: 'Shoes', counter: 20, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'ACCESSORIES',
-              value: { label: 'Accessories', counter: 56, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'WEARABLES',
-              value: { label: 'Wearables', counter: 32, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'FOOD_DRINKS',
-              value: { label: 'Food & Drinks', counter: 15, link: '#' },
-              isLeaf: true,
-            },
-          ],
-        },
-        {
-          key: 'ACTIVITIES',
-          value: { label: 'Activities', counter: 132 },
-          isLeaf: false,
-          children: [
-            {
-              key: 'ALL_ACTIVITIES',
-              value: { label: 'All Activities', counter: 132, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'TRAINING',
-              value: { label: 'Training', counter: 21, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'WORKOUT',
-              value: { label: 'Workout', counter: 43, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'FOOTBALL',
-              value: { label: 'Football', counter: 30, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'FITNESS',
-              value: { label: 'Fitness', counter: 38, link: '#' },
-              isLeaf: true,
-            },
-          ],
-        },
-        {
-          key: 'DEALS',
-          value: { label: 'Deals', counter: 68 },
-          isLeaf: false,
-          children: [
-            {
-              key: 'ALL_DEALS',
-              value: { label: 'All Deals', counter: 68, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'OUTLET',
-              value: { label: 'Outlet', counter: 68, link: '#' },
-              isLeaf: true,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      key: 'KIDS',
-      value: {
-        label: 'Kids',
-        counter: 263,
-        banner: 'https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/toy.png',
-        bannerTitle: 'Unleash your imagination',
-      },
-      isLeaf: false,
-      children: [
-        {
-          key: 'ALL_KIDS',
-          value: { label: 'All Kids', counter: 263, link: '#' },
+          key: 'POPULAR_CLASSICS',
+          value: { label: 'Popular Classics', counter: 120, link: '#' },
           isLeaf: true,
-        },
-        {
-          key: 'CATEGORIES',
-          value: { label: 'Categories', counter: 192 },
-          isLeaf: false,
-          children: [
-            {
-              key: 'ALL_CATEGORIES',
-              value: { label: 'All Categories', counter: 192, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'CLOTHING',
-              value: { label: 'Clothing', counter: 29, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'SHOES',
-              value: { label: 'Shoes', counter: 60, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'ACCESSORIES',
-              value: { label: 'Accessories', counter: 48, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'WEARABLES',
-              value: { label: 'Wearables', counter: 22, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'FOOD_DRINKS',
-              value: { label: 'Food & Drinks', counter: 33, link: '#' },
-              isLeaf: true,
-            },
-          ],
-        },
-        {
-          key: 'ACTIVITIES',
-          value: { label: 'Activities', counter: 40 },
-          isLeaf: false,
-          children: [
-            {
-              key: 'ALL_ACTIVITIES',
-              value: { label: 'All Activities', counter: 40, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'FOOTBALL',
-              value: { label: 'Football', counter: 21, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'BASKETBALL',
-              value: { label: 'Basketball', counter: 19, link: '#' },
-              isLeaf: true,
-            },
-          ],
-        },
-        {
-          key: 'DEALS',
-          value: { label: 'Deals', counter: 31 },
-          isLeaf: false,
-          children: [
-            {
-              key: 'ALL_DEALS',
-              value: { label: 'All Deals', counter: 31, link: '#' },
-              isLeaf: true,
-            },
-            {
-              key: 'OUTLET',
-              value: { label: 'Outlet', counter: 31, link: '#' },
-              isLeaf: true,
-            },
-          ],
         },
       ],
     },
@@ -458,12 +198,13 @@ onMounted(async () => {
           >
             <picture>
               <source
-                srcset="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/vsf_logo_white.svg"
-                media="(min-width: 1024px)"
+                srcset="../img/only_cans.jpg"
+                media="(min-width: 80px)"
               />
               <img
-                src="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/vsf_logo_sign_white.svg"
+                src="../img/only_cans.jpg"
                 alt="Sf Logo"
+                style="width:80px"
               />
             </picture>
           </a>
