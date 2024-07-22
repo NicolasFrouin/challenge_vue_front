561213f306f708cda6d8fcd038e8e45008750a46
Etoiroc <112826661+Etoiroc@users.noreply.github.com>
2024-07-22 16:29:42 +0200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { default as ProductCardDesktop } from './ProductCardDesktop.vue';
+export { default as ProductCardMobile } from './ProductCardMobile.vue';
+export { default as ProductDetails } from './ProductDetails.vue';
+export { default as ProductList } from './ProductList.vue';
