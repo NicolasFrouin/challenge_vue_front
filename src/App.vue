5330a56cbc3c7dd3dad72c6eee7e6a066f8c82ed
YvesMu <95441649+YvesMu@users.noreply.github.com>
2024-07-23 17:09:12 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import { AppFooter, AppHeader } from '@/components/main';
+import { AppFooter, AppHeader, AppLoading } from '@/components/main';
+import PopupVue from '@/components/main/Popup.vue';
+import { onMounted, ref } from 'vue';
 import useAppState from './stores/state';
 import { useRefStore } from './utils/refStore';
+import useAuthStore from './stores/auth';
 
 const { loading } = useRefStore(useAppState());
+//const { jwtLogin } = useRefStore(useAuthStore());//
+
+onMounted(() => {
+  //jwtLogin();//
+});
 </script>
 
 <template>
   <AppHeader />
   <main class="z-10 m-4 relative">
-    <div
-      v-if="loading"
-      class="fixed inset-0 bg-white bg-opacity-95 flex items-center justify-center"
-      :class="{ loading }"
-    >
-      <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary-500"></div>
-    </div>
+    <AppLoading v-if="loading" />
     <RouterView :key="$router.currentRoute.value.fullPath" />
+    <PopupVue />
   </main>
   <AppFooter class="z-30" />
 </template>
 
 <style>
-html:has(.loading) {
-  overflow: hidden;
-  height: 100%;
-}
-
 body:has(.modal-open) {
   overflow: hidden;
 }
