6c9c4272737465dc39546f353052c00d55c8b984
YvesMu <95441649+YvesMu@users.noreply.github.com>
2024-07-24 19:47:55 +0200

    Add files via upload

---
+++ src/utils/cookie.ts
@@ -0,0 +1,18 @@
+export function setCookie(name: string, value: string, minutes: number): void {
+    const d = new Date();
+    d.setTime(d.getTime() + (minutes * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = `${name}=${value};${expires};path=/`;
+  }
+  
+  export function getCookie(name: string): string | null {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+  
