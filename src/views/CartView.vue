2fa46fc4f80f5d0ed2cc065a7d98fb952faafe9e
YvesMu <95441649+YvesMu@users.noreply.github.com>
2024-07-24 18:00:13 +0200

    Update CartView.vue
    Design CartView

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -10,13 +10,26 @@ const { cart } = useRefStore(useCartStore());
 </script>
 
 <template>
-  <div>
-    <div>
-      <RouterLink :to="$router.resolve(routes.checkout)">Checkout</RouterLink>
+  <div class="max-w-4xl mx-auto p-4">
+    <div class="flex justify-end mb-4">
+      <RouterLink 
+        :to="$router.resolve(routes.checkout)"
+        class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+      >
+        Checkout
+      </RouterLink>
     </div>
-    <section class="flex flex-col w-full">
-      <article v-for="cartItem in cart" :key="cartItem.product.id">
-        <ProductCardMobile :product="cartItem.product" :quantity="cartItem.quantity" :type="DisplayType.Cart" />
+    <section class="flex flex-col w-full space-y-4">
+      <article 
+        v-for="cartItem in cart" 
+        :key="cartItem.product.id" 
+        class="bg-white shadow-lg rounded-lg p-4 flex items-center space-x-4"
+      >
+        <ProductCardMobile 
+          :product="cartItem.product" 
+          :quantity="cartItem.quantity" 
+          :type="DisplayType.Cart" 
+        />
       </article>
     </section>
   </div>
